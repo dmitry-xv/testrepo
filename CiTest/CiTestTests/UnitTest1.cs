0159c86e28abb28d51634fff58092a466fa4ef7e
Dmitry Poznyak <dmitry.p@expressvpn.com>
2014-10-22 12:47:48 +0800

    INit v1

---
+++ CiTest/CiTestTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CiTestLib;
+
+namespace CiTestTests
+{
+    [TestClass]
+    public class TestMrWho
+    {
+        [TestMethod]
+        public void Hello(            )
+        {
+            Assert.AreEqual("Hello", new MrWho().SayHello());
+        }
+    }
+}
